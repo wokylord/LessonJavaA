5ef7711d0ae0163c293c540c82ef5a01ef399ef1
Alex <wokylord@gmail.com>
2023-11-15 11:32:43 +0200

    Initial Serializable

---
+++ src/telran/objects/CreationPointsAppl.java
@@ -0,0 +1,9 @@
+package telran.objects;
+
+import java.util.*;
+
+public class CreationPointsAppl {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/telran/objects/Point.java
@@ -0,0 +1,7 @@
+package telran.objects;
+
+import java.io.Serializable;
+
+public record Point(int x, int y)
+implements Serializable {
+}
