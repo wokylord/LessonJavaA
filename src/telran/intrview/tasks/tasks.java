aa3f21610548546481ab9e727b4c764b61abd992
Alex <wokylord@gmail.com>
2023-11-15 09:20:57 +0200

    tasks stream

---
+++ src/telran/intrview/tasks/tasks.java
@@ -0,0 +1,134 @@
+package telran.intrview.tasks;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class tasks {
+    /**
+     *
+     * @param ar array of integer numbers
+     * @param sum integer number
+     * @return true if the given array contains two numbers, the sum of which equals the given sum value
+     */
+    public static boolean isSum2(int [] ar, int sum) {
+
+        boolean running = true;
+        HashSet<Integer> setHelper = new HashSet<>();
+        int i = 0;
+        while(i < ar.length && running) {
+            if(setHelper.contains(sum - ar[i])) {
+                running = false;
+            } else {
+                setHelper.add(ar[i]);
+                i++;
+            }
+        }
+        return !running;
+    }
+    /**
+     *
+     * @param ar array of integer numbers
+     * @param sum integer number
+     * @return true if the given array contains two numbers, the sum of which equals the given sum value
+     */
+    public static boolean isSum2N2(int[] ar, int sum) {
+        int i = 1;
+        int j = 0;
+        boolean running = true;
+        while(i < ar.length && running) {
+            j = i - 1;
+            while(j >= 0 ) {
+                if(ar[j] + ar[i] == sum) {
+                    running = false;
+                    break;
+                }
+                j--;
+            }
+            i++;
+        }
+        return !running;
+    }
+    /**
+     *
+     * @param ar array of integer numbers
+     * @return maximal positive number having negative number with the same absolute value
+     */
+    public static int getMaxPositiveWithNegativeValue (int ar[]) {
+        int res = -1;
+        HashSet<Integer> setHelper = new HashSet<>();
+
+        for(int num: ar) {
+            if(setHelper.contains(-num)) {
+                res = Math.max(res, Math.abs(num));
+            } else {
+                setHelper.add(num);
+            }
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param strings - array of strings
+     * @return Map where key - string, value - number of occurrences in the array
+     */
+    public static HashMap<String, Long> getMapOccurrences(String[] strings) {
+
+        //Optionally, additional bonus if you apply the method "merge" of the interface Map
+        //try to understand this method https://docs.oracle.com/javase/8/docs/api/java/util/Map.html#merge-K-V-java.util.function.BiFunction-
+        //it uses Functional interface BiFunction
+        HashMap<String, Long> res = new HashMap<>();
+        for(String string: strings) {
+            res.merge(string, 1l, Long::sum);
+        }
+        return res;
+    }
+    /**
+     * Anagram of a string is another string containing all the symbols
+     * from the string, but in different order (see the test cases)
+     * @param string
+     * @param anagram
+     * @return true if the second parameter presents an anagram
+     *  of the string from the first parameter, otherwise false
+     */
+    public static boolean isAnagram(String string, String anagram) {
+        boolean res = false;
+        if(!string.equals(anagram) && string.length() == anagram.length()) {
+            HashMap<Character, Integer> mapOccurrences = getStringMap(string);
+            res = anagramCheck(anagram, mapOccurrences);
+        }
+        return res;
+    }
+    private static boolean anagramCheck(String anagram, HashMap<Character, Integer> mapOccurrences) {
+        boolean res = true;
+        char[] symbols = anagram.toCharArray();
+        int i = 0;
+        while (i < symbols.length && res) {
+            if(mapOccurrences.compute(symbols[i], (k, v) -> v==null ? -1 : v - 1) < 0) {
+                res = false;
+            } else {
+                i++;
+            }
+        }
+        return res;
+    }
+    private static HashMap<Character, Integer> getStringMap(String string) {
+        HashMap<Character, Integer> res = new HashMap<>();
+        for(char symbol: string.toCharArray()) {
+            res.merge(symbol, 1, Integer::sum);
+        }
+        return res;
+    }
+    //Task for streams /grouping
+    public static void displayDigitsDistribution() {
+        int nNumbers = 1_000_000;
+        //TODO
+        //create stream of random int's (nNumbers), each int number in range [1, Integer.Max_VALUE)
+        //conversion to stream of Strings
+        //extracting separate char's from Strings
+        //grouping with counting of occurrences
+        //sorting in descending order of occurrences
+        //printing
+    }
+
+}
---
+++ src/telran/intrview/test/interviewTest.java
@@ -0,0 +1,100 @@
+package telran.intrview.test;
+
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static telran.intrview.tasks.tasks.*;
+
+class interviewTest {
+    private static final int N_NUMBERS = 100000;
+    int[] bigArray = new int[N_NUMBERS];
+    @Test
+    @DisplayName("performance O[N] is Sum2 ")
+    void isSum2PerformanceTest() {
+        //isSum2 complexity O[N]
+        isSum2(bigArray, 1);
+    }
+    @Test
+    @Disabled
+    @DisplayName("performance O[N^2] is Sum2N2 ")
+    void isSum2N2PerformanceTest() {
+        //isSum2N2 complexity O[N^2]
+        isSum2N2(bigArray, 1);
+    }
+    @Test
+    @Disabled
+    void isSum2Test() {
+        int [] ar1 = {1000, 300, -200, 20, 40, -10};
+        assertTrue(isSum2(ar1, 800));//1000 + (-200) = 800
+        assertTrue(isSum2(ar1, 990));//1000 + (-10) = 990
+        assertFalse(isSum2(ar1, 50)); //no two number with the given sum
+    }
+    @Test
+    @Disabled
+    void isSum2N2Test() {
+        int [] ar1 = {1000, 300, -200, 20, 40, -10};
+        assertTrue(isSum2N2(ar1, 800));//1000 + (-200) = 800
+        assertTrue(isSum2N2(ar1, 990));//1000 + (-10) = 990
+        assertFalse(isSum2N2(ar1, 50)); //no two number with the given sum
+    }
+    @Test
+    @Disabled
+    void getMaxPositiveWithNegativeValueTest() {
+        int [] ar1 = {-1,100, 200, -50, 1, -100, 50};
+        int [] ar2 = {-1, -100, 200, -500, 10, -100, 50, 200};
+        assertEquals(100, getMaxPositiveWithNegativeValue(ar1));
+        assertEquals(-1,  getMaxPositiveWithNegativeValue(ar2));
+    }
+    @Test
+    @Disabled
+    void getMapOccurrencesTest() {
+        String[] strings = {
+                "lpm", "ab", "a", "lpm", "a", "aa", "lpm"
+        };
+        HashMap<String, Long> mapOccurrences = getMapOccurrences(strings);
+        assertEquals(3, mapOccurrences.get("lpm"));
+        assertEquals(2, mapOccurrences.get("a"));
+        assertEquals(1, mapOccurrences.get("ab"));
+        assertEquals(1, mapOccurrences.get("aa"));
+    }
+    @Test
+    @Disabled
+    void isAnagramTest() {
+        String string = "yellow";
+        assertTrue(isAnagram(string, "wolely"));
+        assertTrue(isAnagram(string, "elolwy"));
+        assertTrue(isAnagram(string, "lowely"));
+        assertTrue(isAnagram(string, "ollwey"));
+        assertFalse(isAnagram(string, "wolelw")); //not y
+        assertFalse(isAnagram(string, string));//the same order
+        assertFalse(isAnagram(string, "wglely"));//g is not from the string
+        assertFalse(isAnagram(string, "wolye"));//must be two 'l'
+
+    }
+    @Test
+    void displayDigitsDistributionTest() {
+        displayDigitsDistribution();
+        //required output
+        /* always 1 will be first
+         *        2 will be second
+         * 1 - <counter of occurrences>
+         * 2 -  <counter of occurrences>
+         * random order of digits
+         * 0 - <counter of occurrences>
+         * 4 - <counter of occurrences>
+         * 3 - <counter of occurrences>
+         * 5 - <counter of occurrences>
+         * 7 - <counter of occurrences>
+         * 6 - <counter of occurrences>
+         * 9 - <counter of occurrences>
+         * 8 - <counter of occurrences>
+         */
+    }
+
+
+}
